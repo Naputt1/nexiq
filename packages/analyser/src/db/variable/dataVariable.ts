8d36981eb4c9dad3001a2a7b7c03da7a55c41745
Naputt1 <>
2026-02-02 03:13:47 +0000

    refactor variable

--- packages/analyser/src/db/variable/dataVariable.ts
+++ packages/analyser/src/db/variable/dataVariable.ts
@@ -1,19 +1,19 @@
 import type { ComponentFileVarNormal, ComponentInfoRender } from "shared";
 import { Variable } from "./variable.js";
+import type { File } from "../fileDB.js";
 
 export class DataVariable extends Variable {
   type: "function" | "data";
   components: Map<string, ComponentInfoRender>;
 
-  constructor({
-    id,
-    name,
-    dependencies,
-    loc,
-    ...options
-  }: Omit<ComponentFileVarNormal, "variableType" | "var" | "components">) {
-    const scope = options.type === "function" ? options.scope : undefined;
-    super(id, name, options.type, dependencies, "normal", loc, scope);
+  constructor(
+    options: Omit<
+      ComponentFileVarNormal,
+      "variableType" | "var" | "components"
+    >,
+    file: File,
+  ) {
+    super({ ...options, variableType: "normal" }, file);
     this.type = options.type;
     this.components = new Map();
   }
@@ -29,6 +29,7 @@ export class DataVariable extends Variable {
   public getData(): ComponentFileVarNormal {
     return {
       ...super.getBaseData(),
+      type: "data",
       variableType: "normal",
       loc: this.loc,
       components: Object.fromEntries(this.components),
--- packages/analyser/src/db/variable/type.ts
+++ packages/analyser/src/db/variable/type.ts
@@ -2,6 +2,7 @@ import type { ComponentVariable } from "./component.js";
 import type { Variable } from "./variable.js";
 import type { DataVariable } from "./dataVariable.js";
 import type { HookVariable } from "./hook.js";
+import type { FunctionVariable } from "./functionVariable.js";
 
 export function isComponentVariable(v: Variable): v is ComponentVariable {
   return v.variableType === "component";
@@ -11,6 +12,14 @@ export function isHookVariable(v: Variable): v is HookVariable {
   return v.variableType === "hook";
 }
 
-export function isDataVariable(v: Variable): v is DataVariable {
+export function isNormalVariable(v: Variable): v is DataVariable {
   return v.variableType === "normal";
+}
+
+export function isFunctionVariable(v: Variable): v is FunctionVariable {
+  return v.type === "function";
+}
+
+export function isDataVariable(v: Variable): v is DataVariable {
+  return v.type === "data";
 }
