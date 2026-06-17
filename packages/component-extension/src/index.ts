7639ea7c88a7fe98efdf700d5c6a0df0b7f422fc
Naputt1 <naputtalt1@gmail.com>
2026-06-17 21:29:37 +0100

    fix: buildin extension config

--- packages/component-extension/src/index.ts
+++ packages/component-extension/src/index.ts
@@ -1,1 +1,14 @@
+import type { Extension } from "@nexiq/extension-sdk";
+import { componentTask } from "./rustTask.js";
+
 export * from "./rustTask.js";
+
+export const componentExtension: Extension = {
+  id: "component-extension",
+  viewTasks: {
+    component: [componentTask],
+    file: [componentTask],
+  },
+};
+
+export default componentExtension;
